55fbaf5933699be26f691d37e37fba66268332ac
byung-u <iam.byungwoo@gmail.com>
2016-12-06 22:38:40 +0900

    Fix read conf file
    Sometimes it doesn't work, becuase sometimes './msg.ini'
    not exist.
    Fixed get path with command.
    Example)
      1. Current path
        % noti_to_wife.py .
        % noti_to_wife.py ./
      2. Absolute path
        % noti_to_wife.py /home/user/test/noti_to_wife/

--- noti_to_wife.py
+++ noti_to_wife.py
@@ -12,34 +12,49 @@ import sys
 
 MAX_SEND_CNT = 3
 
-rv = []
-for i in range(0, MAX_SEND_CNT):
-    rv.append(random.randrange(0, 59))
+def noti_to_wife(conf_path):
+    rv = []
+    for i in range(0, MAX_SEND_CNT):
+        rv.append(random.randrange(0, 59))
+    
+    config = configparser.ConfigParser()
 
-config = configparser.ConfigParser()
-config.readfp(open('./msg.ini'))
-number = config.get('MSG', 'number')
-message = config.get('MSG', 'message')
-command = './send_msg.osa %s %s' % (number, message)
-
-snd_cnt = 0
-while True:
-    time.sleep(1)
-    now = datetime.now()
-
-    # 11, 14, 17
-    if snd_cnt == 0:
-        if (now.hour != 11):
-            continue
-    elif snd_cnt == 1:
-        if (now.hour != 14):
-            continue
-    elif snd_cnt == 2:
-        if (now.hour != 17):
-            continue
+    if (conf_path.endswith('/')):
+        config.readfp(open(conf_path + 'msg.ini'))
     else:
-        sys.exit(1)
+        config.readfp(open(conf_path + '/msg.ini'))
 
-    if (rv[snd_cnt] == now.minute):
-        subprocess.run(command, shell=True, check=True)
-        snd_cnt += 1
+    number = config.get('MSG', 'number')
+    message = config.get('MSG', 'message')
+    command = './send_msg.osa %s %s' % (number, message)
+    
+    snd_cnt = 0
+    while True:
+        time.sleep(1)
+        now = datetime.now()
+    
+        # 11, 14, 17
+        if snd_cnt == 0:
+            if (now.hour != 12):
+                continue
+        elif snd_cnt == 1:
+            if (now.hour != 14):
+                continue
+        elif snd_cnt == 2:
+            if (now.hour != 17):
+                continue
+        else:
+            sys.exit(1)
+    
+        if (rv[snd_cnt] == now.minute):
+            subprocess.run(command, shell=True, check=True)
+            snd_cnt += 1
+    
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage: noti_to_wife.py $CONF_PATH', file=sys.stderr)
+        sys.exit(2)
+
+    conf_path = sys.argv[1]
+    noti_to_wife(conf_path)
+
